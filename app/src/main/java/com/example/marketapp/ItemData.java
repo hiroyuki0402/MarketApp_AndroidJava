114d3aabcf20a43196fd3f7ecc6f08ec62e2e81f
hiroyuki0402 <hiro1990.cloud@gmail.comcd>
2023-10-20 23:23:23 +0900

    2日目 完成

--- app/src/main/java/com/example/marketapp/ItemData.java
+++ app/src/main/java/com/example/marketapp/ItemData.java
@@ -1,16 +1,36 @@
 package com.example.marketapp;
 
-import android.media.Image;
-
+/**
+ * アイテムデータを保持するクラス。
+ * 画像、アイテム名、アイテムの説明をプロパティとして持つ。
+ */
 public class ItemData {
-    // プロパティー
+    // プロパティ
 
+    /**
+     * 画像のリソースID。
+     */
     int imageInt;
+
+    /**
+     * アイテムの名前。
+     */
     String itemName;
+
+    /**
+     * アイテムの説明。
+     */
     String itemDesc;
 
     // コンストラクター
 
+    /**
+     * コンストラクタ。
+     *
+     * @param imageInt  画像のリソースID。
+     * @param itemName  アイテムの名前。
+     * @param itemDesc  アイテムの説明。
+     */
     public ItemData(int imageInt, String itemName, String itemDesc) {
         this.imageInt = imageInt;
         this.itemName = itemName;
@@ -19,26 +39,56 @@ public class ItemData {
 
     // ゲッターセッター
 
+    /**
+     * 画像のリソースIDを取得する。
+     *
+     * @return 画像のリソースID。
+     */
     public int getImageInt() {
         return imageInt;
     }
 
+    /**
+     * 画像のリソースIDを設定する。
+     *
+     * @param imageInt 画像のリソースID。
+     */
     public void setImageInt(int imageInt) {
         this.imageInt = imageInt;
     }
 
+    /**
+     * アイテム名を取得する。
+     *
+     * @return アイテム名。
+     */
     public String getItemName() {
         return itemName;
     }
 
+    /**
+     * アイテム名を設定する。
+     *
+     * @param itemName アイテム名。
+     */
     public void setItemName(String itemName) {
         this.itemName = itemName;
     }
 
+    /**
+     * アイテムの説明を取得する。
+     *
+     * @return アイテムの説明。
+     */
     public String getItemDesc() {
         return itemDesc;
     }
 
+    /**
+     * アイテムの説明を設定する。
+     *
+     * @param itemDesc アイテムの説明。
+     */
     public void setItemDesc(String itemDesc) {
         this.itemDesc = itemDesc;
     }
--- app/src/main/java/com/example/marketapp/MainActivity.java
+++ app/src/main/java/com/example/marketapp/MainActivity.java
@@ -1,14 +1,123 @@
 package com.example.marketapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+/**
+ * メインアクティビティクラス。
+ * RecyclerViewとそのアダプターを設定し、各種アイテムのクリックイベントを処理
+ */
+public class MainActivity extends AppCompatActivity implements ItemClickListener {
+
+    /**
+     * RecyclerViewのインスタンス
+     */
+    RecyclerView recyclerView;
+
+    /**
+     * ヘッダー用のImageView
+     */
+    ImageView imageView;
+
+    /**
+     * フルーツの名前リスト
+     */
+    ArrayList<String> fruitsNames;
+
+    /**
+     * 画像のリソースIDリスト
+     */
+    ArrayList<Integer> imageNames;
+
+    /**
+     * アイテムの説明リスト
+     */
+    ArrayList<String> descriptions;
+
+    /**
+     * アイテムデータリスト
+     */
+    ArrayList<ItemData> itemDatas;
+
+    /**
+     * CustomAdapterのインスタンス
+     */
+    CustomAdapter adapter;
+
+    /**
+     * アクティビティが生成された時の処理
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.reciclerView);
+        imageView = findViewById(R.id.imageView2);
+        imageView.setImageResource(R.drawable.header);
+
+        fruitsItemSetUP();
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        adapter = new CustomAdapter(itemDatas);
+        recyclerView.setAdapter(adapter);
+
+        adapter.setItemClickListener(this);
+    }
+
+    /**
+     * アイテムデータを設定するメソッド
+     */
+    private void fruitsItemSetUP() {
+        fruitsNames = new ArrayList<>();
+        imageNames = new ArrayList<>();
+        descriptions = new ArrayList<>();
+        itemDatas = new ArrayList<>();
+
+        fruitsNames.add("Fruits");
+        fruitsNames.add("Vegetables");
+        fruitsNames.add("Bakery");
+        fruitsNames.add("Beverage");
+        fruitsNames.add("Milk");
+        fruitsNames.add("Snacks");
+
+        descriptions.add("庭から採れた新鮮な果物");
+        descriptions.add("おいしい野菜");
+        descriptions.add("パン、小麦、豆");
+        descriptions.add("ジュース、紅茶、コーヒー、ソーダ");
+        descriptions.add("ミルク、シェイク、ヨーグルト");
+        descriptions.add("ポップコーン、ドーナツ、ドリンク");
+
+        imageNames.add(R.drawable.fruit);
+        imageNames.add(R.drawable.vegitables);
+        imageNames.add(R.drawable.bread);
+        imageNames.add(R.drawable.beverage);
+        imageNames.add(R.drawable.milk);
+        imageNames.add(R.drawable.popcorn);
+
+        for (int i = 0; i < 6; i++) {
+            ItemData data = new ItemData(imageNames.get(i), fruitsNames.get(i), descriptions.get(i));
+            itemDatas.add(data);
+        }
+    }
+
+    /**
+     * アイテムがクリックされた時の処理。
+     */
+    @Override
+    public void onClick(View view, int posion) {
+        Toast.makeText(
+                this,
+                "アイテム " + itemDatas.get(posion).itemName,
+                Toast.LENGTH_SHORT).show();
     }
 }
